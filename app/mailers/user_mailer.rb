21f42b493bed57ae5df064c7e2517d7e3dece38e
Jean-Baptiste Tricheur <jean-baptiste.tricheur@hotmail.fr>
2020-02-18 17:58:13 +0100

    push4

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'gabriel.bouakiar@hotmail.fr'
+  default from: 'jean-baptiste.tricheur@hotmail.fr'
  
   def welcome_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
